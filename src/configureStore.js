6de9c3d144b345335e886229a67806b5b5b144c2
Brian Flannery <bflannery66@email.com>
2020-07-09 14:24:57 -0500

    adds persist on page reload

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,19 +1,30 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
 import rootReducer from './reducers'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export default function configureStore(preloadedState) {
     const middleware = [thunkMiddleware]
     const middlewareEnhancer = applyMiddleware(...middleware)
 
-    const store = createStore(rootReducer, preloadedState, composeWithDevTools(
+    const store = createStore(persistedReducer, preloadedState, composeWithDevTools(
         middlewareEnhancer,
         // other store enhancers if any
     ));
     if (process.env.NODE_ENV !== 'production' && module.hot) {
-        module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
+        module.hot.accept('./reducers', () => store.replaceReducer(persistedReducer))
     }
 
-    return store
+    const persistor = persistStore(store)
+    return { store, persistor }
 }
